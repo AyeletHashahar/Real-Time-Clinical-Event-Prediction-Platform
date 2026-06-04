f410637b3499c5d1d7c15c580af1e8cb73ea5a07
Ayelet Hashahar Cohen <45938000+AyeletHashahar@users.noreply.github.com>
2026-06-04 10:33:56 +0300

    Delete backend/dummy_model.py

--- backend/dummy_model.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-class DummyModel:
-    def __init__(self, name):
-        self.pattern_full_name = name
-
-    def predict_proba(self, feature_matrix, n_steps, mode):
-        return round(random.uniform(0, 1), 3)
